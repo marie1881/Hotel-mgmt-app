d3e2e9a3fee2dd81ffc2e3ad67633c4b1b478933
marie1881 <53557430+marie1881@users.noreply.github.com>
2020-03-05 22:52:46 +0800

    change file names

---
+++ hotel-managment-app/hotel-mgmt-app/client/src/components/layout/Landing.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Landing = () => {
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">Welcome Back!</h1>
+          <p className="lead">
+            Connect with your friendly staff members and stay up to date with
+            the latest information.
+          </p>
+          <div className="buttons">
+            <Link to="/register" className="btn btn-primary">
+              Sign Up
+            </Link>
+            <Link to="/login" className="btn btn-light">
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
