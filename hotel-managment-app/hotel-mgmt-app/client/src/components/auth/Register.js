800af09e83afa8bd4172dc9af770c795ca2e37d8
marie1881 <53557430+marie1881@users.noreply.github.com>
2020-03-06 22:28:00 +0800

    reducers auth middleware

--- hotel-managment-app/hotel-mgmt-app/client/src/components/auth/Register.js
+++ hotel-managment-app/hotel-mgmt-app/client/src/components/auth/Register.js
@@ -20,7 +20,7 @@ const Register = ({ setAlert }) => {
   const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
-      setAlert.setAlert("Passwords do not match", "danger");
+      setAlert("Passwords do not match", "danger");
     } else {
       console.log("Success");
     }
