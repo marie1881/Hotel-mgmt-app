5d48488a5ba2012465ee858cad8475862abff150
marie1881 <53557430+marie1881@users.noreply.github.com>
2020-03-06 23:02:01 +0800

    import

--- hotel-managment-app/hotel-mgmt-app/client/src/components/auth/Register.js
+++ hotel-managment-app/hotel-mgmt-app/client/src/components/auth/Register.js
@@ -2,6 +2,7 @@ import React, { Fragment, useState } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { setAlert } from "../../actions/alert";
+import { register } from "../../actions/auth";
 import PropTypes from "prop-types";
 
 const Register = ({ setAlert }) => {
@@ -90,4 +91,4 @@ Register.propTypes = {
   setAlert: PropTypes.func.isRequired
 };
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(Register);
