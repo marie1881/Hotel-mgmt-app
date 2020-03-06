192fd5ff17c483b06d19e01789058ebc3b482936
marie1881 <53557430+marie1881@users.noreply.github.com>
2020-03-06 18:20:52 +0800

    prop types and redux

--- hotel-managment-app/hotel-mgmt-app/client/src/components/auth/Register.js
+++ hotel-managment-app/hotel-mgmt-app/client/src/components/auth/Register.js
@@ -1,8 +1,10 @@
 import React, { Fragment, useState } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { setAlert } from "../../actions/alert";
+import PropTypes from "prop-types";
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -18,7 +20,7 @@ const Register = () => {
   const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
-      console.log("Passwords do not match");
+      setAlert.setAlert("Passwords do not match", "danger");
     } else {
       console.log("Success");
     }
@@ -84,4 +86,8 @@ const Register = () => {
   );
 };
 
-export default connect()(Register);
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+};
+
+export default connect(null, { setAlert })(Register);
