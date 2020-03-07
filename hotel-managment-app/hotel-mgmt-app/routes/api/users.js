27ab095302defad56288103354a25c02408c116f
marie1881 <53557430+marie1881@users.noreply.github.com>
2020-03-07 23:54:13 +0800

    logout and navbar links

--- hotel-managment-app/hotel-mgmt-app/routes/api/users.js
+++ hotel-managment-app/hotel-mgmt-app/routes/api/users.js
@@ -4,13 +4,13 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
-const { check, validationResult } = require("express-validator/check");
+const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
 
-// @route   POST api/users
-// @desc    Register user
-// @access  Public
+// @route    POST api/users
+// @desc     Register user
+// @access   Public
 router.post(
   "/",
   [
@@ -54,6 +54,7 @@ router.post(
       });
 
       const salt = await bcrypt.genSalt(10);
+
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
