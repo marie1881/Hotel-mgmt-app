1b1e347e4d727410f781a9d6d88ae6a6d30d7faf
marie1881 <53557430+marie1881@users.noreply.github.com>
2020-03-08 18:12:29 +0800

    creating profiles

---
+++ hotel-managment-app/hotel-mgmt-app/client/src/components/profile-form/CreateProfile.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+const CreateProfile = props => {
+  //Minus the experience and education
+  const [formData, setFormData] = useState({
+    company: "",
+    website: "",
+    location: "",
+    status: "",
+    skills: "",
+    githubusername: "",
+    bio: "",
+    twitter: "",
+    facebook: "",
+    linkedin: "",
+    youtube: "",
+    instagram: ""
+  });
+
+  return <div></div>;
+};
+
+CreateProfile.propTypes = {};
+
+export default CreateProfile;
