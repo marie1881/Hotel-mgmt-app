9a0a2d578a95d3bfa5b20a5a3fbf5e5d37d1503a
marie1881 <53557430+marie1881@users.noreply.github.com>
2020-03-08 15:08:29 +0800

    get profile initial

---
+++ hotel-managment-app/hotel-mgmt-app/client/src/reducers/profile.js
@@ -0,0 +1,14 @@
+const initialState = {
+  profile: null,
+  profiles: [],
+  repos: [],
+  loading: true,
+  error: {}
+};
+
+export default function(state = inititalState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+  }
+}
