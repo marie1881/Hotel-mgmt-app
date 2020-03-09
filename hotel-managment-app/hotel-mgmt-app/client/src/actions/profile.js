7da05cd840d1a1d33b3a4362dd949513ca7a374c
marie1881 <53557430+marie1881@users.noreply.github.com>
2020-03-09 22:00:25 +0800

    adding components

--- hotel-managment-app/hotel-mgmt-app/client/src/actions/profile.js
+++ hotel-managment-app/hotel-mgmt-app/client/src/actions/profile.js
@@ -3,6 +3,8 @@ import { setAlert } from "./alert";
 
 import {
   GET_PROFILE,
+  GET_PROFILES,
+  // GET_REPOS,
   PROFILE_ERROR,
   UPDATE_PROFILE,
   CLEAR_PROFILE,
@@ -25,6 +27,58 @@ export const getCurrentProfile = () => async dispatch => {
     });
   }
 };
+
+// Get all profiles
+export const getProfiles = () => async dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
+  try {
+    const res = await axios.get("/api/profile");
+
+    dispatch({
+      type: GET_PROFILES,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.data.msg, status: err.response.status }
+    });
+  }
+};
+
+// Get profiles by ID
+export const getProfileById = userId => async dispatch => {
+  try {
+    const res = await axios.get(`/api/profile/${userId}`);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.data.msg, status: err.response.status }
+    });
+  }
+};
+
+// Get github repos - can be used for something else - If wanted must add GET_REPOS to top and to types.js
+// export const getGithubRepos = userId => async dispatch => {
+//   try {
+//     const res = await axios.get(`/api/profile/github/${username}`);
+
+//     dispatch({
+//       type: GET_REPOS,
+//       payload: res.data
+//     });
+//   } catch (err) {
+//     dispatch({
+//       type: PROFILE_ERROR,
+//       payload: { msg: err.response.data.msg, status: err.response.status }
+//     });
+//   }
+// };
 
 // Create or update profile
 export const createProfile = (
@@ -169,7 +223,7 @@ export const deleteEducation = id => async dispatch => {
 
 // Delete account and profile
 export const deleteAccount = () => async dispatch => {
-  if (windown.comfirm("Are you sure? This can NOT be undone!")) {
+  if (window.confirm("Are you sure? This can NOT be undone!")) {
     try {
       const res = await axios.delete("/api/profile");
 
